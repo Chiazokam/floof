dca1bb14d20fb18c74579c9268d28774da7b2dcd
chiazokam <chiazokamecheta@gmail.com>
2021-08-20 03:54:56 +0100

    modify listening port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 
 import winston from './server/log';
-import env from './server/config/env';
+// import env from './server/config/env';
 import router from './server/routes';
 
 const app = express();
@@ -13,8 +13,8 @@ app.use(express.json());
 
 app.use('/api/v1', router);
 
-app.listen(env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(
-    `⚡️[server]: Server is running at https://localhost:${env.PORT}`,
+    `⚡️[server]: Server is running at https://localhost:${process.env.PORT || 3000}`,
   );
 });
