2db0fb077c56e6f3a3626eab5e53135ebe451973
chiazokam <chiazokamecheta@gmail.com>
2021-08-20 19:34:43 +0100

    Trigger Build

--- server/dataAccess/db/config/config.js
+++ server/dataAccess/db/config/config.js
@@ -1,15 +1,15 @@
 const dotenv = require('dotenv');
-const env = require('../../../config/env');
+// const env = require('../../../config/env');
 
 dotenv.config();
 
 module.exports = {
   development: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
   },
   production: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
