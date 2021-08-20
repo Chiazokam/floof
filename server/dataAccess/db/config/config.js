6c05ec5d39aceb46a8064feb066c0010ecc9c61d
chiazokam <chiazokamecheta@gmail.com>
2021-08-20 12:09:12 +0100

    Trigger Build

--- server/dataAccess/db/config/config.js
+++ server/dataAccess/db/config/config.js
@@ -5,17 +5,7 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: env.DB_USERNAME,
-    password: env.DB_PASSWORD,
-    database: env.DB_DEV,
-    host: env.DB_HOST,
-    dialect: 'postgres',
-  },
-  test: {
-    username: env.DB_USERNAME,
-    password: env.DB_PASSWORD,
-    database: env.DB_TEST,
-    host: env.DB_HOST,
+    url: env.DATABASE_URL,
     dialect: 'postgres',
   },
   production: {
