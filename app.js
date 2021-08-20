898825e8e9dbe872eb6da34c5d087476e74a827d
chiazokam <chiazokamecheta@gmail.com>
2021-08-20 12:29:46 +0100

    Trigger Build

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
 
+import dotenv from 'dotenv';
 import winston from './server/log';
 import env from './server/config/env';
 import router from './server/routes';
 
-const { PORT } = env;
+dotenv.config();
+
+// const { PORT } = env;
 
 const app = express();
 app.use(morgan('combined', { stream: winston }));
@@ -15,9 +18,9 @@ app.use(express.json());
 
 app.use('/api/v1', router);
 
-const APP_PORT = PORT || 3000;
-app.listen(APP_PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log(
-    `⚡️[server]: Server is running on port ${APP_PORT}`,
+    `⚡️[server]: Server is running on port ${PORT}`,
   );
 });
